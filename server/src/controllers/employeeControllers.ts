1d3de0ffdfd2f37521831925e440f82da60da73f
Max Lyubchenko <00eagermax00@gmail.com>
2024-12-17 16:15:28 +0200

    add GET, PUT & DELETE by id in employee controller

--- server/src/controllers/employeeControllers.ts
+++ server/src/controllers/employeeControllers.ts
@@ -16,6 +16,38 @@ export const employeePOST = async (req: Request, res: Response) => {
     const savedEmployee = await newEmployee.save();
     res.status(201).json(savedEmployee);
   } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const employeeGETbyId = async (req: Request, res: Response) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) res.status(404).json({ error: 'Employee not found' });
+    else res.json(employee);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const employeePUTbyId = async (req: Request, res: Response) => {
+  try {
+    const updatedEmployee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedEmployee) res.status(404).json({ error: 'Employee not found' });
+    else res.json(updatedEmployee);
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const employeeDELETEbyId = async (req: Request, res: Response) => {
+  try {
+    const deletedEmployee = await Employee.findByIdAndDelete(req.params.id);
+    if (!deletedEmployee) res.status(404).json({ error: 'Employee not found' });
+    else res.json({ message: 'Employee deleted' });
+  } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 };
