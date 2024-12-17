b35ce79fe01c40c7b452c6bb5a2a8907d700e5d2
Max Lyubchenko <00eagermax00@gmail.com>
2024-12-17 14:51:23 +0200

    add connect to MongoDB Cluster

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,17 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/db';
 
-dotenv.config();
+dotenv.config({ path: '.env.dev' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is working...');
