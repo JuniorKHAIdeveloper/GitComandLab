3986d0ef7c3d27711b5b408a636d9f49e6372787
Max Lyubchenko <00eagermax00@gmail.com>
2024-12-17 16:57:34 +0200

    add errorHendler to middleware

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(`Error: ${err.message}`);
+
+  switch (err.message) {
+    case 'No employees found':
+    case 'Employee not found':
+      res.status(404).json({ message: err.message });
+      break;
+    case 'Position not found':
+      res.status(404).json({ message: err.message });
+      break;
+    case 'Department not found':
+      res.status(404).json({ message: err.message });
+      break;
+    default:
+      res.status(500).json({
+        message: err.message || 'Something went wrong',
+      });
+      break;
+  }
+};
+
+export default errorHandler;
