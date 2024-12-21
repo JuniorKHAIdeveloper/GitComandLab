05fb09f53bc074900847eada25ccbc6d7eaa0245
Max Lyubchenko <00eagermax00@gmail.com>
2024-12-21 20:18:48 +0200

    add OOP in project

--- server/src/controllers/employeeControllers.ts
+++ server/src/controllers/employeeControllers.ts
@@ -1,88 +1,107 @@
 import { NextFunction, Request, Response } from 'express';
-const Employee = require('../models/employee');
+import Employee from '../models/employee';
 
-export const employeeGET = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const query = req.query;
+class EmployeeController {
+  async getEmployees(req: Request, res: Response, next: NextFunction) {
+    try {
+      const query = { ...req.query };
+      const sortBy = query.sortBy as string;
+      const sortOrder = parseInt(query.sortOrder as string, 10);
 
-    const sortBy = req.query.sortBy as string;
-    const sortOrder = parseInt(req.query.sortOrder as string);
+      delete query.sortBy;
+      delete query.sortOrder;
 
-    delete query.sortBy;
-    delete query.sortOrder;
+      let employees;
+      if (sortBy && sortOrder) {
+        employees = await Employee.findEmployees(query);
+        employees.sort((a, b) => (a as any)[sortBy] - (b as any)[sortBy] * sortOrder);
+      } else {
+        employees = await Employee.findEmployees(query);
+      }
 
-    if (sortBy && sortOrder) {
-      const employees = await Employee.find(query)?.sort({ [sortBy]: sortOrder });
-      if (employees.length === 0) next(new Error('No employees found'));
-      else res.json(employees);
-    } else {
-      const employees = await Employee.find(query);
-      if (employees.length === 0) next(new Error('No employees found'));
-      else res.json(employees);
+      if (employees.length === 0) {
+        next(new Error('No employees found'));
+      } else {
+        res.json(employees);
+      }
+    } catch (err: any) {
+      next(err);
     }
-  } catch (err: any) {
-    next(err);
   }
-};
 
-export const employeePOST = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newEmployee = new Employee(req.body);
-    const savedEmployee = await newEmployee.save();
-    res.status(201).json(savedEmployee);
-  } catch (err: any) {
-    next(err);
+  async createEmployee(req: Request, res: Response, next: NextFunction) {
+    try {
+      const savedEmployee = await Employee.createEmployee(req.body);
+      res.status(201).json(savedEmployee);
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
 
-export const employeeGETbyId = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const employee = await Employee.findById(req.params.id);
-    if (!employee) next(new Error('Employee not found'));
-    else res.json(employee);
-  } catch (err: any) {
-    next(err);
+  async getEmployeeById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const employee = await Employee.findEmployeeById(req.params.id);
+      if (!employee) {
+        next(new Error('Employee not found'));
+      } else {
+        res.json(employee);
+      }
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
 
-export const employeePUTbyId = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updatedEmployee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!updatedEmployee) next(new Error('Employee not found'));
-    else res.json(updatedEmployee);
-  } catch (err: any) {
-    next(err);
+  async updateEmployeeById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const updatedEmployee = await Employee.updateEmployeeById(req.params.id, req.body);
+      if (!updatedEmployee) {
+        next(new Error('Employee not found'));
+      } else {
+        res.json(updatedEmployee);
+      }
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
 
-export const employeeDELETEbyId = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const deletedEmployee = await Employee.findByIdAndDelete(req.params.id);
-    if (!deletedEmployee) next(new Error('Employee not found'));
-    else res.json({ message: 'Employee deleted' });
-  } catch (err: any) {
-    next(err);
+  async deleteEmployeeById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const deletedEmployee = await Employee.deleteEmployeeById(req.params.id);
+      if (!deletedEmployee) {
+        next(new Error('Employee not found'));
+      } else {
+        res.json({ message: 'Employee deleted' });
+      }
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
 
-export const departmentGET = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const department = await Employee.distinct('department');
-    if (!department) next(new Error('Department not found'));
-    else res.json(department);
-  } catch (err: any) {
-    next(err);
+  async getDepartments(req: Request, res: Response, next: NextFunction) {
+    try {
+      const departments = await Employee.getDistinct('department');
+      if (!departments.length) {
+        next(new Error('Departments not found'));
+      } else {
+        res.json(departments);
+      }
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
 
-export const positionGET = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const position = await Employee.distinct('position');
-    if (!position) next(new Error('Position not found'));
-    else res.json(position);
-  } catch (err: any) {
-    next(err);
+  async getPositions(req: Request, res: Response, next: NextFunction) {
+    try {
+      const positions = await Employee.getDistinct('position');
+      if (!positions.length) {
+        next(new Error('Positions not found'));
+      } else {
+        res.json(positions);
+      }
+    } catch (err: any) {
+      next(err);
+    }
   }
-};
+}
+
+export default new EmployeeController();
